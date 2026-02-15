052719003fd35d64b41ed0450c183d8191b65226
Danii44 <danishahmad444@gmail.com>
2026-02-16 02:56:42 +0400

    refrtesh

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,31 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  // Ignore build and next folders
+  globalIgnores(['dist', '.next', 'node_modules']),
   {
     files: ['**/*.{ts,tsx}'],
+    // Extended configs for JS, TS, and React Hooks
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
+      ...tseslint.configs.recommended,
+      reactHooks.configs.recommended,
     ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.node, // Added Node globals for Next.js SSR
+      },
+    },
+    rules: {
+      // You can add custom rules here
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
     },
   },
 ])
